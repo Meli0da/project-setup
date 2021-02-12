5fb06081ecba0485d81f5f4f6c6bdbe1720d9ecf
Meli0da <jessi-h.mail@web.de>
2021-02-12 13:08:48 +0100

    Images + Hero Image + Unterseiten
    Saubererer Code
    Integration Images
    Hero image
    Hinzufügen Unterseiten

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const copyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -40,30 +39,96 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico|webmanifest|xml)$/i,
+        type: 'asset/resource'
+      },    
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      title: "Desgin System von jessica-hein.de",
-      description:"Sammlung der Designinhalte von jessica-hein.de.",
-      template: "src/design-system.html",
-      filename: "design-system.html",
-    }),
     new HtmlWebpackPlugin({
       title: "Illustrator, Grafik- und Webdesigner Jessica Hein",
       description:"Jessica Hein, Dein professioneller Illustrator, Grafik- und Webdesigner.",
       template: "src/index.html",
       filename: "index.html",
+      favicon: "src/favicons/favicon.ico",
     }),
-    new copyWebpackPlugin({
+    new HtmlWebpackPlugin({
+      title: "Mehr über Jessica Hein",
+      description:"Jessica Hein; Werdegang, Motivation und Hobbies.",
+      template: "src/ueber-mich.html",
+      filename: "ueber-mich.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Leistungen von Jessica Hein",
+      description:"Illustrationen, Grafik- und Webdesigns sind teil des Leistungsspektrums von Jessica Hein.",
+      template: "src/leistungen.html",
+      filename: "leistungen.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Portfolio von Jessica Hein",
+      description:"Das Portfolio von Jessica Hein enthält diverse digitale und Printprojekte.",
+      template: "src/portfolio.html",
+      filename: "portfolio.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Kontaktaufnahme mit Jessica Hein",
+      description:"Jetzt mit Jessica Hein Kontakt aufnehmen und zusammen spannende Designs realisieren.",
+      template: "src/kontakt.html",
+      filename: "kontakt.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Datenschutz von Jessica Hein",
+      description:"Hier findest du die Datenschutzrichtlinien von Jessica Hein.",
+      template: "src/datenschutz.html",
+      filename: "datenschutz.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Impressum von Jessica Hein",
+      description:"Hier findest du das Impressum von Jessica Hein.",
+      template: "src/impressum.html",
+      filename: "impressum.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Danke für Deine Nachricht ",
+      description:"Danke für die Kontaktaufnahme! Du hast das Formular erfolgreich abgesendet.",
+      template: "src/danke.html",
+      filename: "danke.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Fehler 404, Seite nicht gefunden",
+      description:"Fehler 404, die Seite die Du suchst wurde nicht gefunden.",
+      template: "src/error404.html",
+      filename: "error404.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Desgin System von jessica-hein.de",
+      description:"Sammlung der Designinhalte von jessica-hein.de.",
+      template: "src/design-system.html",
+      filename: "design-system.html",
+      favicon: "src/favicons/favicon.ico",
+    }),
+    new CopyWebpackPlugin({
       patterns: [
         {
-        from: "src/images",
-         to: "images",
+          from: "src/images",
+          to: "images",
         },
         {
-        from: "src/favicons", 
-        to: "favicons"
+          from: "src/favicons", 
+          to: "favicons"
+        },
+        {
+          from: "src/icons", 
+          to: "icons"
         }
       ] 
     }),
